a852f26a4025ec78ed543e0d437d2e04c92b10dd
carneirofc <claudiofcarneiro@hotmail.com>
2019-02-27 09:57:14 -0300

    network and hostname handlers.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
-
 import Monitor from "./components/Monitor";
 
-import { getNodes, rebootNode } from './api.js';
+import { reIpNetworkIp, reHostname } from './regex';
+import { getNodesAll, postNodeReboot, postNodeIpNetworkGateway, postNodeHostname } from './api/node';
 
 import "./App.css";
 
@@ -18,8 +18,8 @@ class App extends Component {
         };
         this.state = {
             currentWindow: this.window.MONITOR,
-            baseUrl: 'https://10.0.38.34/bbb-daemon/api',
-            refreshInterval: 1000,
+            baseUrl: 'http://0.0.0.0:4850',
+            refreshInterval: 2000,
             showSettings: false,
             nodes: [],
             online: false
@@ -37,7 +37,7 @@ class App extends Component {
 
     loadData = async (app) => {
         // Make a request
-        getNodes(app.state.baseUrl,
+        getNodesAll(app.state.baseUrl,
             (response) =>
                 app.setState({
                     nodes: response.data,
@@ -53,8 +53,36 @@ class App extends Component {
         }
     };
 
+    /** Handle calls to configure a node's hostname. */
+    handleNewHostname = (ip, hostname) =>{
+        hostname = hostname.trim();
+        if(reHostname.test(hostname)){
+            console.log('New hostname', hostname);
+            postNodeHostname(this.props.baseUrl, ip, hostname);
+            return true;
+        }else{
+            console.log('Does not match the pattern.');
+            return false;
+        }
+    }
+
+    /** Handle calls to configure a node's network. */
+    handleNewIp = (ip, new_ip_string, fThen, fCatch) =>{
+        // Tries to match the ip net_ip/mask ip pattern
+        if(reIpNetworkIp.test(new_ip_string)){
+            // ip_new, ip_network, ip_gateway
+            let aux = new_ip_string.split(" ");
+            console.log('New network config', aux);
+            postNodeIpNetworkGateway(this.props.baseUrl, ip, aux[0].trim(), aux[1].trim(), aux[2].trim())
+            return true;
+        }else{
+            console.log('Does not match the pattern.');
+            return false;
+        }
+    };
+
     handleReboot = (ip) => {
-        rebootNode(this.props.baseUrl, ip);
+        postNodeReboot(this.props.baseUrl, ip);
     }
 
     handleUpdateUrl = (newBaseUrl) => {
@@ -68,7 +96,7 @@ class App extends Component {
             return { showSettings: !(prevState.showSettings === true) };
         });
     }
-    
+
     renderWindow = (currentWindow) => {
         switch (currentWindow) {
             case this.window.MONITOR:
@@ -76,6 +104,8 @@ class App extends Component {
                     appStates={this.state}
                     appProps={this.props}
                     appFunctions={{
+                        handleNewHostname: this.handleNewHostname,
+                        handleNewIp: this.handleNewIp,
                         handleUpdateUrl: this.handleUpdateUrl,
                         handleDisplaySettings: this.handleDisplaySettings,
                         handleWindowChange: this.handleWindowChange,
@@ -87,7 +117,7 @@ class App extends Component {
     };
 
     handleReboot = (ip) => {
-        rebootNode(this.state.baseUrl, ip);
+        postNodeReboot(this.state.baseUrl, ip);
     }
 
     render() {
